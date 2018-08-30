d6f00db367ccb379364bc574e277e8ec2051cc22
MarcelHimmelreich <marcel.himmelreich@uni-weimar.de>
2018-08-30 13:34:24 +0200

    Add Scene and Shader Manager

--- Assets/Scripts/ShaderManager.cs
+++ Assets/Scripts/ShaderManager.cs
@@ -2,13 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GraphManager : MonoBehaviour {
-    //View
-    public GameObject UserInterface;
-    //Model
-    public GameObject Model;
-    //Shader Manager
-    public GameObject ShaderManager;
+public class ShaderManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
